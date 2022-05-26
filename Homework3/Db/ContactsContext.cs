b2bbb2aaafa6a598783b7aacdf9501400bc9c8ff
Mike Tate <shagee@uw.edu>
2022-05-26 12:53:06 -0700

    Completed homework 3 assignment

---
+++ Homework3/Db/ContactsContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Homework3.Db
+{
+    public partial class ContactsContext : DbContext
+    {
+        public ContactsContext()
+        {
+        }
+
+        public ContactsContext(DbContextOptions<ContactsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Contacts;integrated security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.ToTable("Contact");
+
+                entity.Property(e => e.ContactCreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ContactEmail).HasMaxLength(50);
+
+                entity.Property(e => e.ContactName).HasMaxLength(50);
+
+                entity.Property(e => e.ContactNotes).HasMaxLength(1000);
+
+                entity.Property(e => e.ContactPhoneNumber).HasMaxLength(50);
+
+                entity.Property(e => e.ContactPhoneType).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,39 @@
+﻿var context = new Homework3.Db.ContactsContext();
+
+const string CONTACT_NAME = "Mike";
+
+var existingContact = context.Contacts.FirstOrDefault(c => c.ContactName == CONTACT_NAME);
+
+if (existingContact == null)
+{
+    existingContact = new Homework3.Db.Contact()
+    {
+        ContactAge = 40,
+        ContactCreatedDate = DateTime.Now,
+        ContactName = "Mike",
+        ContactEmail = "mike@uw.edu",
+        ContactNotes = "This is a test note",
+        ContactPhoneNumber = "206-999-8888",
+        ContactPhoneType = "mobile"
+    };
+    context.Contacts.Add(existingContact);
+    context.SaveChanges();
+    Console.WriteLine("Added the contact.");
+}
+else
+{
+    Console.WriteLine($"The contact record for {CONTACT_NAME} already exists.");
+}
+
+Console.WriteLine($"Info for {existingContact.ContactName}:");
+Console.WriteLine($"Age: {existingContact.ContactAge}");
+Console.WriteLine($"Email: {existingContact.ContactEmail}");
+Console.WriteLine($"Phone: {existingContact.ContactPhoneNumber} ({existingContact.ContactPhoneType})");
+Console.WriteLine($"Notes: {existingContact.ContactNotes}");
+Console.WriteLine($"Created: {existingContact.ContactCreatedDate.ToShortDateString()}");
+
+context.Contacts.Remove(existingContact);
+context.SaveChanges();
+Console.WriteLine("Removed the contact.");
+
+Console.WriteLine("Number of contact records: " + context.Contacts.Count());
